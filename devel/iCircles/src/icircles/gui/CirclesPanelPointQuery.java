e0e183bbf69b43808642b49996dcd7ec6e336642
Jean Flower <jeanflower@rocketmail.com>
2011-11-05 16:02:15 +0000

    Adding new query classes.

---
+++ devel/iCircles/src/icircles/gui/CirclesPanelPointQuery.java
@@ -0,0 +1,49 @@
+package icircles.gui;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+import icircles.concreteDiagram.CircleContour;
+import icircles.concreteDiagram.ConcreteSpider;
+import icircles.concreteDiagram.ConcreteSpiderFoot;
+import icircles.concreteDiagram.ConcreteZone;
+
+public class CirclesPanelPointQuery {
+
+	private ConcreteDiagramPointQuery cdpq;
+	
+	CirclesPanelPointQuery(CirclesPanel cp, Point2D.Double p, double tol)
+	{
+		this.cdpq = new ConcreteDiagramPointQuery(cp.getDiagram(), p, tol);
+	}
+	ArrayList<CircleContour> getContours()
+	{
+		return cdpq.getContours();
+	}
+	ConcreteZone getConcreteZone()
+	{
+		return cdpq.getConcreteZone();
+	}
+	class SpiderInfo
+	{
+		ConcreteSpiderFoot foot;
+		ConcreteSpider spider;
+		SpiderInfo(ConcreteSpider s, ConcreteSpiderFoot f)
+		{
+			foot = f;
+			spider = s;
+		}
+		
+	}
+	SpiderInfo getSpider()
+	{
+		ConcreteSpider s = cdpq.getSpider();
+		if(s != null)
+		{
+			ConcreteSpiderFoot foot = cdpq.getSpiderFoot();
+			SpiderInfo result = new SpiderInfo(s, foot);
+			return result;
+		}
+		return null;
+	}
+	}
---
+++ devel/iCircles/src/icircles/gui/ConcreteDiagramPointQuery.java
@@ -0,0 +1,124 @@
+package icircles.gui;
+
+import icircles.concreteDiagram.CircleContour;
+import icircles.concreteDiagram.ConcreteDiagram;
+import icircles.concreteDiagram.ConcreteSpider;
+import icircles.concreteDiagram.ConcreteSpiderFoot;
+import icircles.concreteDiagram.ConcreteZone;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class ConcreteDiagramPointQuery {
+
+	static int INSIDE = 0;
+	static int COINCIDENT = 1;
+	static int OUTSIDE = 2;
+
+	private ConcreteDiagram cd;
+	
+	private HashMap<CircleContour, Integer> containmentInfo;
+	private ConcreteSpider s;
+	private ConcreteSpiderFoot foot;
+	
+	ConcreteDiagramPointQuery(ConcreteDiagram cd, Point2D.Double p, double tol)
+	{
+		this.cd = cd;
+		containmentInfo = new HashMap<CircleContour, Integer>();
+		for(CircleContour cc : cd.getCircles())
+		{
+			double dist = Math.sqrt((p.x - cc.get_cx())*(p.x - cc.get_cx()) + 
+        					         (p.y - cc.get_cy())*(p.y - cc.get_cy())); 
+			if(dist < cc.get_radius() - tol )
+				containmentInfo.put(cc, INSIDE);
+			else if (dist > cc.get_radius() + tol )
+				containmentInfo.put(cc, OUTSIDE);
+			else
+				containmentInfo.put(cc, COINCIDENT);
+		}
+		boolean done = false;
+		for(ConcreteSpider s : cd.getSpiders())
+		{
+			if(done)
+				break;
+			for(ConcreteSpiderFoot f : s.feet)
+			{
+				double dist = Math.sqrt((p.x - f.x)*(p.x - f.x) + 
+										(p.y - f.y)*(p.y - f.y)); 
+				if(dist < f.footRad + tol )
+				{
+					this.s = s;
+					this.foot = f;
+					done = true;
+					break;
+				}
+			}
+		}
+	}
+	
+	ArrayList<CircleContour> getContours()
+	{
+		ArrayList<CircleContour> result = new ArrayList<CircleContour>();
+		for(CircleContour cc : cd.getCircles())
+		{
+			Integer i = containmentInfo.get(cc);
+			if(i.intValue() == COINCIDENT)
+				result.add(cc);
+		}		
+		return result;
+	}
+	private ConcreteZone selectZoneFromList(ArrayList<ConcreteZone> list)
+	{
+		for(ConcreteZone cz : list)
+		{
+			boolean containment_ok = true;
+			for(CircleContour cc : cz.getContainingContours())
+			{
+				Integer i = containmentInfo.get(cc);
+				if(i.intValue() != INSIDE)
+					{
+					containment_ok = false;
+					break;
+					}
+			}
+			if(containment_ok)
+			{
+				for(CircleContour cc : cz.getExcludingContours())
+				{
+					Integer i = containmentInfo.get(cc);
+					if(i.intValue() != OUTSIDE)
+						{
+						containment_ok = false;
+						break;
+						}
+				}
+			}
+			if(containment_ok)
+			{
+				return cz;
+			}
+		}
+		return null;
+	}
+	public ConcreteZone getConcreteZone()
+	{
+		ConcreteZone z = selectZoneFromList(cd.getShadedZones());
+		if(z != null)
+			return z;
+		z = selectZoneFromList(cd.getUnshadedZones());
+		return z;
+	}
+
+	public ConcreteSpider getSpider() {
+		return s;
+	}
+
+	public ConcreteSpiderFoot getSpiderFoot() {
+		return foot;
+	}
+}
+
+
+
